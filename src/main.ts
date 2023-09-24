b2be954933c846a7f1fb8f1a8ed41b24adc6beac
Alan Grainger <alan.grainger@gmail.com>
2023-09-24 19:57:01 +0200

    Cleanup

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,7 @@ export default class AirQuotes extends Plugin {
             new Notice('No source path found in YAML/frontmatter. Make sure to link to your source text.')
             return
           }
+
           // Attempt to resolve the link text into a matching TFile
           const bookFile = app.metadataCache.getFirstLinkpathDest(bookPath, view.file.path)
           if (bookFile) {
@@ -46,6 +47,7 @@ export default class AirQuotes extends Plugin {
     })
 
     // Import an ePub into your Vault as a new Markdown note
+    // This is not available on mobile
     if (Platform.isDesktop) {
       this.addCommand({
         id: 'convert-epub',
@@ -58,10 +60,13 @@ export default class AirQuotes extends Plugin {
             // I was unable to find any unzip solution that works for mobile.
             const Epub = require('./Epub').Epub
             const doc = new Epub(this, file.path || '')
+
+            // Conver the provided file to Markdown
             const filename = await doc.convertToMarkdown()
+
+            // Insert the link to the converted file if we're in editing mode
             const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView)
             if (filename && markdownView) {
-              // Insert the link to the converted file
               await app.fileManager.processFrontMatter(markdownView.file, frontMatter => {
                 frontMatter[this.settings.bookSourceVariable] = '[[' + filename.slice(0, -3) + ']]'
               })
